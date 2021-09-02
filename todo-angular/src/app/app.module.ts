fc9e43030c045c3f3202aeaffe4cce97507e965e
Joseph Palma <43054953+josephpalma@users.noreply.github.com>
2021-09-01 19:26:15 -1000

    todos

--- todo-angular/src/app/app.module.ts
+++ todo-angular/src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+//requires forms module for adding a new todo
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { TodosComponent } from './components/todos/todos.component';
 
-@NgModule({
+@NgModule({ 
+  //where all components are declared for use in the app
   declarations: [
-    AppComponent
+    AppComponent,
+    TodosComponent
   ],
+  //where all imported modules are loaded for use in the app
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
