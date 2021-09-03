b3819d4787979af876af20d9891215477383fdbb
Joseph Palma <43054953+josephpalma@users.noreply.github.com>
2021-09-02 15:39:36 -1000

    finishing touches

--- todo-angular/src/app/components/todos/todos.component.ts
+++ todo-angular/src/app/components/todos/todos.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Todo } from './../../models/todo';
+import { GetApiService } from 'src/app/get-api.service';
 
 @Component({
   selector: 'app-todos',
@@ -14,15 +15,20 @@ export class TodosComponent implements OnInit {
 
   //imports todo class as a Type
   todos: Todo[];
+  data: any;
 
   inputTodo: string = "";
 
-  constructor() { 
+  constructor(private api:GetApiService) { 
     this.todos = [];
+    this.data = {};
   }
 
   //on component mount
   ngOnInit(): void {
+    this.api.apiCall().subscribe((data) => {
+      this.data = data;
+    });
     this.todos = [
       {
         content: 'First todo',
@@ -50,9 +56,13 @@ export class TodosComponent implements OnInit {
     this.todos = this.todos.filter((value, i) => i !== id);
   }
 
+  // Deletes all todos from list
+  deleteAll() {
+    this.todos = [];
+  }
+
   // Pushes a new todo to the list, clears form
   addTodo() {
-
     if (this.inputTodo === "") {
       return null;
     } else {
@@ -68,4 +78,24 @@ export class TodosComponent implements OnInit {
     }
   }
 
+  // Populates todos with random stuff from random API
+  populateList() {
+    this.deleteAll();
+    for (let i = 0; i < 3; i++) {
+      console.log("inserting: ", this.data[this.randomInt()].title)
+      let random = this.randomInt();
+      console.log("\n random int: ", random)
+      console.log("\n i: ", i)
+
+      this.todos.push({
+        content: this.data[this.randomInt()].title,
+        completed: false
+      });
+      
+    }
+  }
+
+  randomInt() {
+    return Math.floor(Math.random() * 100);
+  }
 }
